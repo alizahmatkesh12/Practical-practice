278d303a8b108ead1cfc573847aee7c0dabe58da
alizm1241 <156007530+alizahmatkesh12@users.noreply.github.com>
2024-05-10 21:40:47 +0330

    Update is_isomorphic.py

--- is_isomorphic.py
+++ is_isomorphic.py
@@ -1,22 +1,20 @@
-
-
-
-
-
-def is_isomorphic(str1,str2):
-    if len(str1) != len(str2):
+def is_isomorphic(s, t):
+    if len(s) != len(t):
         return False
     
     dict = {}
     set_values = set()
-    
-    for i in range(len(str1)):
-        if str1 not in dict:
-            dict[str1[i]] = str2[i]
-            set_values.add(str2[i])
-        
-    return dict, set_values
 
-print( is_isomorphic('foo', 'bar'))    
-        
-        
+    for i in range(len(s)):
+        if s[i] not in dict:
+            if t[i] in set_values:
+                return False
+            dict[s[i]] = t[i]
+            set_values.add(t[i])
+        else:
+            if dict[s[i]] != t[i]:
+                return False
+    return True
+
+
+print(is_isomorphic("fow","baa"))
